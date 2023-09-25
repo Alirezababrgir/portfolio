9bbc036eab6183d73b4e5e728b7b54fe5c2b22bc
Alirezababrgir <zakhargithub021@gmail.com>
2023-09-25 15:48:06 -0400

    create navbar by appbar and toolbarn -130

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
 import './App.css';
-import { Button } from '@mui/material';
-import { FaBeer } from "react-icons/fa";
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import rtlPlugin from 'stylis-plugin-rtl';
 import { CacheProvider } from '@emotion/react';
 import createCache from '@emotion/cache';
 import { prefixer } from 'stylis';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-
-
-const theme = createTheme({ //create theme for mui rtl
-  direction: 'rtl',
-  typography:'Vazir,Roboto'
-});
+import  Appbar  from './components/UI/Appbar';
 
 const cacheRtl = createCache({
   key: 'muirtl',
   stylisPlugins: [prefixer, rtlPlugin],
 });
 
+const darkTheme = createTheme({//create darktheme for mui rtl
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#1976d2',
+    },
+    direction: 'rtl',
+    typography:'Vazir,Roboto'
+  },
+});
+
+
 function App() {
   return (
     <CacheProvider value={cacheRtl}>
-      <ThemeProvider theme={theme}>
+      <ThemeProvider theme={darkTheme}>
         <HelmetProvider>
           <Helmet>
             <title>بیوگرافی من | صفحه اصلی</title>
           </Helmet>
-          <div className='App'>
-            <p>سلام پی<FaBeer /></p>
-            <Button variant='contained'>کلیک کن</Button>
-          </div>
+          <Appbar/>
+          {[...new Array(120)]
+            .map(
+              () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`,
+            )
+            .join('\n')}
         </HelmetProvider>
       </ThemeProvider>
     </CacheProvider>
---
+++ src/components/UI/Appbar.jsx
@@ -0,0 +1,31 @@
+import { cloneElement } from "react";
+import { AppBar, Toolbar, Button, useScrollTrigger } from "@mui/material";
+
+const Appbar = () => {
+  function ElevationScroll(props) {
+    const { children } = props;
+
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0,
+    });
+
+    return cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+  }
+
+  return (
+    <>
+      <ElevationScroll>
+        <AppBar>
+          <Toolbar>
+            <Button variant="primary">کلیک کن</Button>
+          </Toolbar>
+        </AppBar>
+      </ElevationScroll>
+    </>
+  );
+};
+
+export default Appbar;
