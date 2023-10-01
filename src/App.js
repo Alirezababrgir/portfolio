38c929361c64cd5494538f08d911a1c3baa4aa64
Alirezababrgir <zakhargithub021@gmail.com>
2023-10-01 12:29:25 -0400

    create hambergermenu in mobile size by fab tag -149

--- src/App.js
+++ src/App.js
@@ -1,31 +1,48 @@
 import './App.css';
 import MainLayout from './components/UI/layuot/Mainlayout';
 import Sidebar from './components/UI/Sidebar';
+import Sidebardrawer from './components/UI/Sidebardrawer';
 import Mcontent from './components/maincontent';
 import { useState } from 'react';
 import Tabpanel from './components/tabs/Tabpanel';
 import { Drawer } from '@mui/material';
+import Fab from '@mui/material/Fab';
+import DensityMediumIcon from '@mui/icons-material/DensityMedium';
+import { Box } from '@mui/material';
 
 function App() {
   const [value, setValue] = useState(0);
-  const [draweropen, setdraweropen] = useState(false)
+  const [draweropen, setdraweropen] = useState(false);
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
+
+  const handlechangetoggle = () => {
+    setdraweropen (draweropen=>!draweropen);
+  }
+
+
   return (
     <MainLayout>
+       <Box sx={{display:{xs:"block",md:"none"},m:2}}>
+        <Fab onClick={handlechangetoggle} variant="extended" color='primary' size="large" aria-label='sidebar'>
+          <DensityMediumIcon />
+        </Fab>
+      </Box>
       <Sidebar value={value} handleChange={handleChange} />
       <Drawer
         open={draweropen}
         variant="temporary"
         onClose={setdraweropen}
+        anchor="left"
         sx={{
           "& .MuiPaper-root": {
-            width: 400
-          }
+            width: 300
+          },display:{xs:"block",md:"none"}
         }}
       >
-        <Sidebar value={value} handleChange={handleChange} />
+        <Sidebardrawer value={value} handleChange={handleChange} handlechangetoggle={handlechangetoggle}/>
       </Drawer>
       <Mcontent>
         <Tabpanel value={value} index={0}>
--- src/components/UI/Sidebardrawer.jsx
+++ src/components/UI/Sidebardrawer.jsx
@@ -9,9 +9,11 @@ import TerminalRoundedIcon from '@mui/icons-material/TerminalRounded';
 import TextSnippetRoundedIcon from '@mui/icons-material/TextSnippetRounded';
 import CopyrightIcon from '@mui/icons-material/Copyright';
 import PowerRoundedIcon from '@mui/icons-material/PowerRounded';
+import Fab from '@mui/material/Fab';
+import { Box } from '@mui/material';
+import KeyboardDoubleArrowLeftRoundedIcon from '@mui/icons-material/KeyboardDoubleArrowLeftRounded';
 
-
-const Sidebar = ({ value, handleChange }) => {
+const Sidebardrawer = ({ value, handleChange,handlechangetoggle}) => {
     const tabProps = (index) => {
         return {
             id: `sidebar-tab-${index}`,
@@ -19,8 +21,13 @@ const Sidebar = ({ value, handleChange }) => {
         }
     };
     return (
-        <Grid id="Gr" item md={3} sx={{ backgroundColor: "gray.main", padding: "2rem", display: { xs: "none", md: "block" }, borderRight: `4px solid #777` }} color={grey[100]}>
-            <Avatar sx={{ width: "150px", height: "150px", margin: "0 auto", display: { xs: "none", md: "block" } }} variant="circular" src='https://toplearn.com/img/user/250x259/2402cc6d-1d17-6a22-e6cc-39e3248f13a4_%D8%A7%DB%8C%D9%85%D8%A7%D9%86_%D9%85%D8%AF%D8%A7%D8%A6%D9%86%DB%8C9.jpg' />
+        <Grid id="Gr" item md={3} sx={{ backgroundColor: "gray.main", padding: "2rem", borderRight: `4px solid #777` }} color={grey[100]}>
+            <Box sx={{ display: { xs: "block", md: "none" }, m: 2 }}>
+                <Fab onClick={handlechangetoggle} variant="extended" color='primary' size="large" aria-label='sidebar'>
+                    <KeyboardDoubleArrowLeftRoundedIcon/>
+                </Fab>
+            </Box>
+            <Avatar sx={{ width: "110px", height: "110px", margin: "0 auto" }} variant="circular" src='https://toplearn.com/img/user/250x259/2402cc6d-1d17-6a22-e6cc-39e3248f13a4_%D8%A7%DB%8C%D9%85%D8%A7%D9%86_%D9%85%D8%AF%D8%A7%D8%A6%D9%86%DB%8C9.jpg' />
             <Divider variant="middle" sx={{ mt: 4, mb: 2, color: "white" }} color={grey[600]} />
             <Typography variant="caption" sx={{ textAlign: "center" }} color={grey[500]}>توسعه دهنده فول استک وب</Typography>
             <br />
@@ -32,11 +39,10 @@ const Sidebar = ({ value, handleChange }) => {
                 <Tab icon={<TerminalRoundedIcon />} iconPosition='start' label="نمونه کارها"{...tabProps(2)} />
                 <Tab icon={<Groups2RoundedIcon />} iconPosition='start' label="درباره من"{...tabProps(3)} />
                 <Tab icon={<PowerRoundedIcon />} iconPosition='start' label=" ارتباط با من"{...tabProps(4)} />
-
             </Tabs>
             <Divider variant="middle" sx={{ mt: 2, mb: 2, color: "white" }} color={grey[600]} />
             <Typography variant="subtitle2" sx={{ textAlign: "center" }} color={grey[700]}>توسعه یافته توسط zakhar <CopyrightIcon sx={{ color: grey[500] }} /> </Typography>
         </Grid>
     )
 }
-export default Sidebar;
+export default Sidebardrawer;
