10e7c8ad7e91943976cf1828fd9544e0dbad2dc7
Alirezababrgir <zakhargithub021@gmail.com>
2023-10-12 09:51:18 -0400

    text animation by react-text-transion tool -167

--- src/components/pages/homepage.jsx
+++ src/components/pages/homepage.jsx
@@ -8,6 +8,9 @@ import Particles from "react-particles";
 import { loadSlim } from "tsparticles-slim";
 import { Part1 } from "../../particels/particels";
 import WavingHandRoundedIcon from '@mui/icons-material/WavingHandRounded';
+import TextTransition, { presets } from 'react-text-transition';
+import { useEffect, useState } from "react";
+
 const Homepage = () => {
 
 
@@ -21,31 +24,39 @@ const Homepage = () => {
     }, []);
 
 
+
+    const [index, setIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(
+            () => setIndex((index) => index + 1),
+            3000, // every 3 seconds
+        );
+        return () => clearTimeout(intervalId);
+    }, []);
+    const TEXTS = ['توسعه دهنده فول استک ', 'توسعه دهنده بلاکچین  ', 'عاشق لینوکس هستم'];
+
+
     return (
 
         <>
             <Box sx={{ p: 3 }}>
                 <Particles id="tsparticles" options={Part1} init={particlesInit} loaded={particlesLoaded} />
-                <Divider variant="middle" textAlign="right" sx={{ mt: 2, mb: 2 ,"&::before,&::after":{borderColor:"black"} }}><Typography sx={{ color: "black"}}>سلام <WavingHandRoundedIcon sx={{fontSize:"larger"}}/> </Typography></Divider>
+                <Divider variant="middle" textAlign="right" sx={{ mt: 2, mb: 2, "&::before,&::after": { borderColor: "black" } }}><Typography sx={{ color: "black" }}>سلام <WavingHandRoundedIcon sx={{ fontSize: "larger" }} /> </Typography></Divider>
             </Box>
-            <Box sx={{ backgroundColor:"#2222",height: "40%", width: "60%", borderRadius: "0.5rem", margin: " 0 auto", display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center",mt:8 }}>
+            <Box sx={{ backgroundColor: "#2222", height: "40%", width: "60%", borderRadius: "0.5rem", margin: " 0 auto", display: "flex", justifyContent: "center", flexDirection: "column", alignItems: "center", mt: 8 }}>
                 <FingerprintIcon color={"primary"} sx={{ height: "100px", width: "100px", mb: 4 }} />
                 <Typed
-                    strings={[':) سلام من  عیرضا ببرگیر هستم']}
+                    strings={['( : سلام من علیرضا هستم']}
                     typeSpeed={40}
                 />
                 <br />
-                <Typed
-                    strings={[
-                        ' توسعه دهنده فول استک وب ',
-                        ' توسعه دهنده قراردادهای هوشمند ',
-                        '   و عاشق لینوکس هستم']}
-                    startDelay={3000}
-                    showCursor={false}
-                    typeSpeed={50}
-                    backSpeed={50}
-                    loop >
-                </Typed>
+                <Box sx={{ display: "flex" }}>
+                    <TextTransition springConfig={presets.wobbly}>{TEXTS[index % TEXTS.length]}</TextTransition>
+                    <Typography sx={{ mx: 0.6}}>
+                        من 
+                    </Typography>
+                </Box>
             </Box>
         </>
 
