20d9e6275c13b87445addb30eab9d421c9616aae
Alirezababrgir <zakhargithub021@gmail.com>
2023-10-23 13:00:18 -0400

    redesign resoumepage

--- src/components/pages/resoumetimeline.jsx
+++ src/components/pages/resoumetimeline.jsx
@@ -1,9 +1,7 @@
 import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
-import { Alarm, Dangerous, FaceTwoTone, Laptop } from '@mui/icons-material';
-import { SignLanguageOutlined } from '@mui/icons-material';
 import { useTheme } from '@emotion/react';
-import { InsertEmoticonRounded,School,InfoRounded,TerminalRounded,TrendingDownRounded,CurrencyBitcoinOutlined,HubRounded,WbIncandescentRounded,HandymanRounded,NetworkPingRounded,LightRounded} from '@mui/icons-material';
+import { InsertEmoticonRounded, School,  TerminalRounded, TrendingDownRounded,  HubRounded, WbIncandescentRounded, HandymanRounded} from '@mui/icons-material';
 import {
   SiSolidity,
   SiNodedotjs,
@@ -25,31 +23,37 @@ import {
   SiWeb3Dotjs,
   SiAlchemy,
   SiVisualstudiocode,
+  SiTarget
 
 } from "react-icons/si";
+import { blue, green, grey, orange, red, yellow } from '@mui/material/colors';
+
 const MYtimeline = () => {
+
+  const mytheme = useTheme()
+
   return (
-    <VerticalTimeline lineColor='#111'>
+    <VerticalTimeline lineColor='#fff'>
       <VerticalTimelineElement
         className="vertical-timeline-element--work"
-        contentStyle={{ background: '#4C7D9F', color: '#fff' }}
+        contentStyle={{ backgroundColor: "#4c7d9f" }}
         contentArrowStyle={{ borderRight: '7px solid  #4C7D9F' }}
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: '#fff' }}
-        icon={<InsertEmoticonRounded />}
+        iconStyle={{ background:  mytheme.palette.mode === "dark" ? blue[800] : blue[500], color: mytheme.palette.mode === "dark" ? grey[200] : grey[800]}}
+        icon={<SiTarget/>}
       >
-        <h3 className="vertical-timeline-element-title"> از گچ کاری  تا  بلاکچین</h3>
+        <h3 className="vertical-timeline-element-title"> از گچ کاری  تا امنیت بلاکچین</h3>
         <h4 className="vertical-timeline-element-subtitle">مرور ۱۰ سال</h4>
         <p>
           اسکرول کنید تا با رزومه من به اهمیت دانش پی ببرید
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        contentStyle={{ backgroundColor: "#999", color: "text.primary"  }}
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
         contentArrowStyle={{ borderRight: '7px solid #999' }}
         className="vertical-timeline-element--work"
         date="2010 - 2011"
-        iconStyle={{ background: 'rgb(33, 150, 243)', color: "text.primary"  }}
-        icon={<School/>}
+        iconStyle={{ background: '#999', color: blue[900] }}
+        icon={<School />}
       >
         <h3 className="vertical-timeline-element-title"> ترک تحصیل</h3>
         <h4 className="vertical-timeline-element-subtitle">دیپلم تجربی</h4>
@@ -58,26 +62,26 @@ const MYtimeline = () => {
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        contentStyle={{ background: '#999', color: 'text.primary' }}
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
         contentArrowStyle={{ borderRight: '7px solid  #999' }}
         className="vertical-timeline-element--work"
         date="2008 - 2010"
-        iconStyle={{ background: '#999', color: '#fff' }}
-        icon={<HandymanRounded/>}
+        iconStyle={{ background: '#999', color:yellow[600] }}
+        icon={<HandymanRounded />}
       >
         <h3 className="vertical-timeline-element-title">کارگری ساختمان </h3>
         <h4 className="vertical-timeline-element-subtitle"> گچ کاری</h4>
         <p>
-مجبور بودم برای کمک به خانواده کارگری کنم
+          مجبور بودم برای کمک به خانواده کارگری کنم
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        contentStyle={{ background: '#999', color: 'InfoText' }}
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
         contentArrowStyle={{ borderRight: '7px solid  #999' }}
-        icon={<HubRounded/>}
+        icon={<HubRounded />}
         className="vertical-timeline-element--work"
         date="2006 - 2008"
-        iconStyle={{ background: '#999', color: '#fff' }}
+        iconStyle={{ background: '#999', color: '#4c7d9f' }}
       >
         <h3 className="vertical-timeline-element-title">پا دویی و بازاریابی </h3>
         <h4 className="vertical-timeline-element-subtitle">پا دویی بازار و بازاریابی شبکه ای</h4>
@@ -86,12 +90,12 @@ const MYtimeline = () => {
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        contentStyle={{ background: '#999', color: 'WindowText' }}
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
         contentArrowStyle={{ borderRight: '7px solid  #999' }}
-        icon={<TrendingDownRounded/>}
+        icon={<TrendingDownRounded />}
         className="vertical-timeline-element--education"
         date="April 2013"
-        iconStyle={{ background: '#999', color: 'text.primary' }}
+        iconStyle={{ background: '#999', color: red[500] }}
 
       >
         <h3 className="vertical-timeline-element-title">سقوط</h3>
@@ -101,26 +105,26 @@ const MYtimeline = () => {
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        contentStyle={{ background: '#999', color: '#fff' }}
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
         contentArrowStyle={{ borderRight: '7px solid  #999' }}
-        icon={<WbIncandescentRounded/>}
+        icon={<WbIncandescentRounded />}
         className="vertical-timeline-element--education"
         date="November 2012"
-        iconStyle={{ background: '#999', color: '#fff' }}
+        iconStyle={{ background: '#999', color: orange[400] }}
       >
         <h3 className="vertical-timeline-element-title">بازگشت</h3>
         <h4 className="vertical-timeline-element-subtitle">اما نه به شکل قبل</h4>
         <p>
-         بعد از تحویل مغازه بازار طلبکار ها به سراغم اومدن و همزمان به چند دادگاه ارجا شدم همون حین یکی از اشناها یک بازار یابی بهم معرفی کرد که اینبار فرق داشت پلتفرم ان توسط قراد داد هوشمند کنترل و تضمین میشد و پاداش ان به صورت رمزارز بین بازاریاب ها داده میشد همان هنگام بقول معروف لامپم روشن شد ارررره خودشه همونه که میخواستم . من محو قدرت بلاکچین شده بودم چون نه تنها تحریم بلکه مشکل اعتماد هم میشه توسط قرارداد هوشمند تضمین کرد دیگه هیچ چیز نمیتونه هیچجوره کسب و کارمو تهدید کنه . این همون چیزی بود که میخواستم یعنی امنیت شغلی و خوشبختانه تونستم از فعالیت در همین بازاریابی پول طلبکارهارو پرداخت کنم و رضایت اونارو جلب کنم
+          بعد از تحویل مغازه بازار طلبکار ها به سراغم اومدن و همزمان به چند دادگاه ارجا شدم همون حین یکی از اشناها یک بازار یابی بهم معرفی کرد که اینبار فرق داشت پلتفرم ان توسط قراد داد هوشمند کنترل و تضمین میشد و پاداش ان به صورت رمزارز بین بازاریاب ها داده میشد همان هنگام بقول معروف لامپم روشن شد ارررره خودشه همونه که میخواستم . من محو قدرت بلاکچین شده بودم چون نه تنها تحریم بلکه مشکل اعتماد هم میشه توسط قرارداد هوشمند تضمین کرد دیگه هیچ چیز نمیتونه هیچجوره کسب و کارمو تهدید کنه . این همون چیزی بود که میخواستم یعنی امنیت شغلی و خوشبختانه تونستم از فعالیت در همین بازاریابی پول طلبکارهارو پرداخت کنم و رضایت اونارو جلب کنم
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        contentStyle={{ background: '#999', color: '#fff' }}
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
         contentArrowStyle={{ borderRight: '7px solid  #999' }}
         className="vertical-timeline-element--education"
         date="2002 - 2006"
-        icon={<TerminalRounded/>}
-        iconStyle={{ background: '#999', color: '#fff' }}
+        icon={<TerminalRounded />}
+        iconStyle={{ background: '#999', color: green[800] }}
       >
         <h3 className="vertical-timeline-element-title">برنامه نویسی</h3>
         <h4 className="vertical-timeline-element-subtitle">بدون وقت تلف کردن</h4>
@@ -129,18 +133,67 @@ const MYtimeline = () => {
         </p>
       </VerticalTimelineElement>
       <VerticalTimelineElement
-        contentStyle={{ background: '#999', color: '#fff' }}
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
         contentArrowStyle={{ borderRight: '7px solid  #999' }}
-
+        icon={<SiJavascript />}
         className="vertical-timeline-element--education"
         date="2002 - 2006"
-        iconStyle={{ background: '#999', color: '#fff' }}
+        iconStyle={{ background: '#999', color: yellow[700] }}
       >
 
-        <h3 className="vertical-timeline-element-title">Bachelor of Science in Interactive Digital Media Visual Imaging</h3>
-        <h4 className="vertical-timeline-element-subtitle">Bachelor Degree</h4>
+        <h3 className="vertical-timeline-element-title">یادگیری فرانت</h3>
+        <h5 className="vertical-timeline-element-subtitle">بیسیک</h5>
+        <br />
+        <h4 className="vertical-timeline-element-subtitle">Html5,Css3,flexbox,Javascript(Es6),Bootstrap,Ajax(axios,xml,jquery)</h4>
         <p>
-          Creative Direction, Visual Design
+          رفتم سراغ ویدیو های انلاین و شروع کردم به یادگیری و حدود دوسالی تمرین جاوااسکریپت وبوت استرپ کردم و با متد های اچ تی تی پی داده های جیسون رو از منبع های اماده دریافت و ارسال میکردم که خیلی برام لذت بخش بود اونموقع ولی خب کاملا با مفهوم پرامیس ها و جاوااسکریپت نامتقارن اشنا شدم
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
+        contentArrowStyle={{ borderRight: '7px solid  #999' }}
+        icon={<SiSolidity />}
+        className="vertical-timeline-element--education"
+        date="2002 - 2006"
+        iconStyle={{ background: '#999', color: "#333"}}
+      >
+
+        <h3 className="vertical-timeline-element-title">یادگیری سالیدیتی</h3>
+        <h4 className="vertical-timeline-element-subtitle">اما ناموفق</h4>
+        <p>
+          طبق نقشه راهی که از استاید و منابع انلاین داشتم حس کردم وقتشه برم سراغ سالیدیتی بلافاصله توی یک بوت کمپ حضوری با عنوان بوت کمپ برنامه نویسی بلاکچین ثبت نام کردم اما خیلی از از نکات رو متوجه نمیشدم مثل نود پکیج و خیلی از اصطلاحات دیگ رو و علاوه بر ان نیاز دیدم حداقل با یکی از فریم ورک های جاوااسکریپت  باید  بلد باشم کار کنم چه برسه به فریم ورک ها و کتابخانه های سالیدیتی خلاصه به این درک رسیدم تا نتونم یک پلیکیشن وب ۲ رو پیاده سازی کنم نمیتونم سراغ وب ۳ برم
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
+        contentArrowStyle={{ borderRight: '7px solid  #999' }}
+        icon={<SiReact />}
+        className="vertical-timeline-element--education"
+        date="2002 - 2006"
+        iconStyle={{ background: '#999', color: blue[800] }}
+      >
+        <h3 className="vertical-timeline-element-title">یادگیری فرانت</h3>
+        <h4 className="vertical-timeline-element-subtitle">پیشرفته </h4>
+        <br />
+        <h4 className="vertical-timeline-element-subtitle">React.js,Redux,Next.js,MaterialUi,...</h4>
+        <p>
+          بلافاصله رفتم سراغ ری اکت وبعد از گذروندن دوره انلاین  ۲۰۰ ساعته صفر تا صد ری اکت بهمراه تریال دیزاین تونستم اپلیکیشن های زیبایی رو خلق کنم اما دوست داشتم نکست هم یاد بگیر تا بتونم اپلیکشن های بهینه تری که قابلیت رندر صفحات وب رو روی سرور دارند رو پیاده سازی کنم پس دوره انلاین ۱۰۰ ساعته صفر تا صد نکست رو هم گذروندم و حالا دیگه متوجه شده بودم برنامه نویسی حفظ کردن کد ها نیست بلکه کد ها در داکیومنت موجوده فقط باید بتونم داکیومنت بخونم
+        </p>
+      </VerticalTimelineElement>
+      <VerticalTimelineElement
+        contentStyle={{ backgroundColor: mytheme.palette.mode === "dark" ? grey[800] : "#999", color: mytheme.palette.mode === "dark" ? grey[200] : grey[800] }}
+        contentArrowStyle={{ borderRight: '7px solid  #999' }}
+        icon={<SiNodedotjs />}
+        className="vertical-timeline-element--education"
+        date="2002 - 2006"
+        iconStyle={{ background: '#999', color: green[800] }}
+      >
+        <h3 className="vertical-timeline-element-title">یادگیری بک</h3>
+        <h4 className="vertical-timeline-element-subtitle">صفرتاصد </h4>
+        <br />
+        <h4 className="vertical-timeline-element-subtitle">node.js</h4>
+        <p>
+          حالا دیگه میتونستم هر اپلیکیشنی که میخوام رو بسازم وحالا دیگه به وب ۲ هم علاقه پیدا کرده بودم اما مشکلی وجود داشت ! اون هم اینکه داده ها در اپلیکیشن من واقعی نبودند و صرفا ابجکت هایی بودن که تو فایل جیسون اپلیکشن ذخیره کرده بودم پس ه فکر نوشتن کد سمت سرور افتادم و چه زبانی بهتر از جاوااسکریپت که باهاش بسیار راحتم پس سراغ نود جی اس رفتم
         </p>
       </VerticalTimelineElement>
     </VerticalTimeline>
