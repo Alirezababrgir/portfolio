f533159935b4083c3b773d4bfb0ed77971394d97
Alirezababrgir <zakhargithub021@gmail.com>
2023-12-17 19:21:02 -0500

    set png avatar in HomeContent

--- src/components/pages/Homecontent.jsx
+++ src/components/pages/Homecontent.jsx
@@ -2,6 +2,7 @@ import { SiLinux } from "react-icons/si";
 import { Avatar, Box } from "@mui/material";
 import TextTransition, { presets } from "react-text-transition";
 import { ArrowBack } from "@mui/icons-material";
+import img1 from "../../assetes/images (1) (1).png"
 
 const Homecontent = ({ index }) => {
 
@@ -14,10 +15,12 @@ const Homecontent = ({ index }) => {
 
     return (
         <Box class="card">
-            <Box class="thumbnail scale-up-center "><Avatar class="left" src="https://toplearn.com/img/user/250x259/2402cc6d-1d17-6a22-e6cc-39e3248f13a4_%D8%A7%DB%8C%D9%85%D8%A7%D9%86_%D9%85%D8%AF%D8%A7%D8%A6%D9%86%DB%8C9.jpg" /></Box>
+            <Box class="thumbnail scale-up-center ">
+                <Avatar class="left" src={img1} />
+            </Box>
             <Box class="right">
                 <h1>
-                    <ArrowBack/>
+                    <ArrowBack />
                     <TextTransition style={{ fontSize: "30px" }} springConfig={presets.wobbly}>{TEXTS[index % TEXTS.length]}</TextTransition>
                 </h1>
             </Box>
